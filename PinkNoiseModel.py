e4ec3d78a609d773197ae264211742b71c612eb2
Spencer Powers <spencer.powers197@gmail.com>
2020-07-02 12:41:29 -0700

    Made each model type its own file to make development easier

---
+++ PinkNoiseModel.py
@@ -0,0 +1,74 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from GeometricModel import GeometricModel
+from Wire import *
+from helpers import generateBipartiteGraph
+from StraightWireModel import StraightWireModel
+
+# PinkNoiseModel class: defines a model that uses points generated
+# via pink noise to define random 5th-order polynomials that are
+# rotated about a random point to produce realistic nanowires
+class PinkNoiseModel(StraightWireModel):
+
+    # call inherited constructor
+    def __init__(self, density, r_e, alpha, num_e, numPointsPerWire):
+        GeometricModel.__init__(self, density, r_e, alpha, num_e)
+
+        # new attribute: number of points per wire
+        self.numPoints = numPointsPerWire
+
+        # generate wires
+        self.generateWires()
+
+    # generate wire models
+    def generateWires(self):
+
+        for wire in range(self.num_w):
+
+            # initialize new wire
+            newWire = PinkNoiseWire(wire, self.numPoints) 
+
+            # set scaled points (both x and y) and apply another transformation
+            # on the y-values (see paper)
+            newWire.setScaledPoints(self.length)
+
+            # add wire to list
+            self.Wires.append(newWire)
+
+            # update list of connected electrodes
+            # implement later
+
+    # override plot method for geometric model (electrodes and wires)
+    def plotModel(self):
+
+        # initialize figure
+        plt.figure(1)
+        ax = plt.gca()
+        
+        # loop over electrodes
+        for electrode in range(self.num_e):
+            coords = self.ElectrodeList[electrode].getCenter()
+            rad = self.ElectrodeList[electrode].getRadius()
+            # add circle object
+            circle = plt.Circle((coords[0], coords[1]), rad, color='r')
+            ax.add_artist(circle)
+
+        # loop over wires
+        for wire in range(self.num_w):
+            x = self.Wires[wire].getRotatedXPoints()
+            y = self.Wires[wire].getRotatedYPoints()
+            plt.plot(x, y, color='blue')
+
+        plt.xlim(0, self.length)
+        plt.ylim(0, self.length)
+        plt.savefig('GeometricModelPlot.png')
+        #plt.close()
+        plt.show()
+        
+        
+
+
+        
+        
+
--- Wire.py
+++ Wire.py
@@ -3,6 +3,7 @@ from scipy.ndimage import gaussian_filter
 import numpy as np
 import colorednoise as cn
 from Electrode import Electrode
+import matplotlib.pyplot as plt
 # Wire class - defines attributes of various wire models
 
 class StraightWire:
@@ -163,31 +164,81 @@ class PinkNoiseWire(StraightWire):
         tempMax = max(self.rawPoints)
         # copy the raw points
         self.scaledPoints = self.rawPoints[:]
-        for point in self.scaledPoints:
-            point = (length * (point - tempMin)) / (tempMax - tempMin)
+        # scale them to the correct range
+        for i in range(self.numPoints):
+            self.scaledPoints[i] = (length * (self.scaledPoints[i] - tempMin)) / (tempMax - tempMin)
 
         # set scaled x values while you're at it
-        self.scaledXPoints = np.linspace(0, length, len(self.scaledPoints))
+        # note that I had to extend the x range to ensure that the ends of the wire
+        # did not end up in the work area after rotation
+        bufferLength = (length * (math.sqrt(2) - 1)) / 2
+        self.scaledXPoints = np.linspace(-1 * bufferLength, length + bufferLength, 
+                len(self.scaledPoints))
 
+        #print("after initial scaling: y min/max = ", min(self.scaledPoints), " ", 
+        #        max(self.scaledPoints))
+        """
+        plt.plot(self.scaledXPoints, self.scaledPoints)
+        plt.xlim(-1 * bufferLength, length + bufferLength)
+        plt.ylim(-1 * bufferLength, length + bufferLength)
+        plt.show()
+        """
+        
         # do secondary transformation of y values
-        for point in self.scaledPoints:
-            r = np.random.uniform()
-            point = point * (r ** 3) # from paper
+        for i in range(self.numPoints):
+            r = np.random.uniform(0, 1)
+            self.scaledPoints[i] = self.scaledPoints[i] * (r ** 1) # from paper
 
+        #print("after multiplying by r^3: y min/max = ", min(self.scaledPoints), " ", 
+        #        max(self.scaledPoints))
+        """
+        plt.plot(self.scaledXPoints, self.scaledPoints)
+        plt.xlim(0, length)
+        plt.ylim(0, length)
+        plt.show()
+        """
+        
         # do tertiary transformation of y values
         tempMin = min(self.scaledPoints)
         tempMax = max(self.scaledPoints)
-        for point in self.scaledPoints:
-            v = np.random.uniform(tempMin, tempMax)
-            point = point + v
+        # temp copy for comparison later
+        tempCopy = self.scaledPoints.copy()
+        for i in range(self.numPoints):
+            #print("point before: ", point)
+            v = np.random.uniform(-1 * tempMin, length - tempMax)
+            #print("v = ", v)
+            self.scaledPoints[i] = self.scaledPoints[i] + v
+            #print("point after: ", point)
+        #print("did anything change? diff vector: ", tempCopy - self.scaledPoints)
 
+        #print("after adding v: y min/max = ", min(self.scaledPoints), " ", 
+        #        max(self.scaledPoints))
+        
+        """
+        plt.plot(self.scaledXPoints, self.scaledPoints)
+        plt.xlim(0, length)
+        plt.ylim(0, length)
+        plt.show()
+        """
+        
         # feed y-values through Gaussian filter function (per paper)
         # guess 1 for SD..not listed in paper
-        self.scaledPoints = gaussian_filter(self.scaledPoints, sigma=1)
+        self.scaledPoints = gaussian_filter(self.scaledPoints, sigma=3)
+
+        # interested to see what the range of y values is after filtering
+        #print("after filtering: y min/max = ", min(self.scaledPoints), " ", 
+        #        max(self.scaledPoints))
 
         # fit quintic polynomial to data
         poly_coeff = np.polyfit(self.scaledXPoints, self.scaledPoints, 5)
 
+        """
+        plt.plot(self.scaledXPoints, self.scaledPoints)
+        plt.xlim(0, length)
+        plt.ylim(0, length)
+        plt.show()
+        """
+        
         # store the function output at each x in place of the scaled y-values
         for i in range(self.numPoints):
             self.scaledPoints[i] = self.quintic(poly_coeff, self.scaledXPoints[i])
@@ -197,20 +248,73 @@ class PinkNoiseWire(StraightWire):
         theta = np.random.uniform(0, 2 * math.pi)
         x_r = length / 2
         y_r = length / 2
-        self.x_rotated = np.zeros(self.numPoints)
-        self.y_rotated = np.zeros(self.numPoints)
-        for i in range(self.numPoints):
-            self.x_rotated[i] = (self.scaledXPoints[i] - x_r) * math.cos(theta) - \
-                    (self.scaledPoints[i] - y_r) * math.sin(theta) + x_r
-            self.y_rotated[i] = (self.scaledXPoints[i] - x_r) * math.sin(theta) + \
-                    (self.scaledPoints[i] - y_r) * math.cos(theta) + y_r
+        #self.x_rotated, self.y_rotated = self.rotateXY(self.scaledXPoints, 
+        #        self.scaledPoints, x_r, y_r, theta)
+        # don't rotate for now
+        self.x_rotated = self.scaledXPoints
+        self.y_rotated = self.scaledPoints
+        #print("After rotation:")
+        #print("x min/max: ", min(self.x_rotated), " ", max(self.x_rotated))
+        #print("y min/max: ", min(self.y_rotated), " ", max(self.y_rotated))
 
+        """
+        plt.plot(self.x_rotated, self.y_rotated)
+        plt.xlim(0, length)
+        plt.ylim(0, length)
+        plt.show()
+        """
+        
+
+
+        # random test - remove later
+        """
+        t_test = np.arange(0, math.pi, math.pi/30)
+        x_test = np.zeros(len(t_test))
+        y_test = np.zeros(len(t_test))
+        for t in range(len(t_test)):
+            x_test[t] = 1.5 + 1.5 * math.cos(t_test[t])
+            y_test[t] = 1.5 * math.sin(t_test[t])
+        print("min/max x: ", min(x_test), " ", max(x_test))
+        print("min/max y: ", min(y_test), " ", max(y_test))
+        plt.plot(x_test, y_test)
+        plt.xlim(0, 3)
+        plt.ylim(0, 3)
+        plt.show()
+
+        # rotate the random test
+        x_rot, y_rot = self.rotateXY(x_test, y_test, x_r, y_r, 0.01)
+        plt.plot(x_rot, y_rot)
+        plt.xlim(0, 3)
+        plt.ylim(0, 3)
+        plt.show()
+        """ 
+
+    # helper function for rotating points about another point by an angle
+    def rotateXY(self, x, y, x_r, y_r, theta):
+        # initialize arrays of the same size (will get overwritten)
+        r_x = x.copy()
+        r_y = y.copy()
+        for i in range(len(x)):
+            # apply rotation matrix operation to x coordinate
+            r_x[i] = (x[i] - x_r) * math.cos(theta) - \
+                    (y[i] - y_r) * math.sin(theta) + x_r
+            
+            # apply rotation matrix operation to y coordinate
+            r_y[i] = (x[i] - x_r) * math.sin(theta) + \
+                    (y[i] - y_r) * math.cos(theta) + y_r
+        return r_x, r_y
         
     # helper function for the quintic polynomial
     def quintic(self, coeff_vec, x_in):
-        return coeff_vec[0] * (x_in ** 5) + coeff_vec[1] * (x_in ** 4) + coeff_vec[2] * \
-                (x_in ** 3) + coeff_vec[3] * (x_in ** 2) + coeff_vec[4] * (x_in) + coeff_vec[5]
+        return (coeff_vec[0] * (x_in ** 5) + coeff_vec[1] * (x_in ** 4) + coeff_vec[2] * \
+                (x_in ** 3) + coeff_vec[3] * (x_in ** 2) + coeff_vec[4] * (x_in) + coeff_vec[5])
 
+    # helper function for the quintic polynomial
+    def sept(self, coeff_vec, x_in):
+        return (coeff_vec[0] * (x_in ** 7) + coeff_vec[1] * (x_in ** 6) + coeff_vec[2] * \
+                (x_in ** 5) + coeff_vec[3] * (x_in ** 4) + coeff_vec[4] * (x_in ** 3) + \
+                coeff_vec[5] * (x_in ** 2) + coeff_vec[6] * (x_in) + coeff_vec[7])
+        
     # getter for final rotated x points
     def getRotatedXPoints(self):
         return self.x_rotated
